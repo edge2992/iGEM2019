6e49949201d3b7d48149221c9ea493d0a0a3c5ad
kaz-i-54 <torune.summer@gmail.com>
2019-08-24 08:30:08 +0900

    グラフ小数点二桁２苦戦

--- Young/Young_check.py
+++ Young/Young_check.py
@@ -1,4 +1,6 @@
-from Young.Young_pattern import Young_Pattern
+import cv2
+
+from Young.Young_pattern import Young_Pattern, is_visible
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -37,7 +39,8 @@ class Young_Check(Young_Pattern):
         plotを出力して収束を確認する
         """
         x = [0]
-        y = [self.init_state(100, 100).sum()]
+        # y = [self.init_state(100, 100).sum()]
+        y = [self.state.sum()]
         # self.show()
         for i in range(1, 30):
             y.append(self.sum(1))
@@ -45,6 +48,37 @@ class Young_Check(Young_Pattern):
         # self.show()
         plt.plot(x, y, 'ro')
         plt.show()
+
+    def show_cv2(self):
+        winname = "Young Pattern"
+        # ret = 0
+        wait = 50
+
+        while True:
+            img = self.to_image()
+            cv2.imshow(winname, img)
+            ret = cv2.waitKey(wait)
+            self.next_generation()
+            # prop_val = cv2.getWindowProperty(winname, cv2.WND_PROP_ASPECT_RATIO)
+            if ret == ord('r'):
+                self.init_state(init_alive_prob=0.08)
+            if ret == ord('s'):
+                wait = min(wait * 2, 1000)
+            if ret == ord('f'):
+                wait = max(wait // 2, 10)
+            if ret == ord('q') or ret == 27:
+                break
+            if not is_visible(winname):
+                break
+            if ret == ord('w'):
+                self.save_text("../data/save.txt")
+            if ret == ord('l'):
+                self.load_text("../data/save.txt")
+        cv2.waitKey(1)  # macの都合
+        # cv2.destroyWindow('finger print')
+        cv2.destroyAllWindows()
+        # cv2.waitKey()  # macの都合
+        return 0
 
 
 def heat_map(para1_name, para1, para2_name, para2):
@@ -56,8 +90,8 @@ def heat_map(para1_name, para1, para2_name, para2):
     :param para2: 実験するパラメーターの幅のarray
     :return:
     """
-    pd.options.display.float_format = '{:.2f}'.format
-    pd.options.display.float_format = '{:.2f}'.format
+    pd.options.display.float_format = '{:.1f}'.format
+    pd.options.display.float_format = '{:.1f}'.format
     param_df = ({'r1': 3,
                  'r2': 6,
                  'w1': 1.0,
@@ -72,8 +106,7 @@ def heat_map(para1_name, para1, para2_name, para2):
         for bb in para2:
             param_df[para1_name] = aa
             param_df[para2_name] = bb
-            # print(param_df.values)
-            tmp_se = pd.Series([aa, bb,
+            tmp_se = pd.Series(['{:.2f}'.format(aa), '{:.2f}'.format(bb),
                                 Young_Check(param_df['r1'], param_df['r2'], param_df['w1'], param_df['w2'],
                                             param_df['init_alive_prob']).check(25, 25)],
                                index=[para1_name, para2_name, 'score'])
@@ -83,9 +116,9 @@ def heat_map(para1_name, para1, para2_name, para2):
     print(type(param_df['w1']))
     df_pivot = pd.pivot_table(data=df, values='score',
                               columns=para1_name, index=para2_name, aggfunc=np.mean)
-    sns.heatmap(df_pivot, cmap='Blues', annot=True, fmt='.2f')
-    plt.gca().yaxis.set_major_formatter(plt.FormatStrFormatter('%.1f'))  # y軸小数点以下1桁表示
-    plt.gca().xaxis.set_major_formatter(plt.FormatStrFormatter('%.1f'))  # y軸小数点以下1桁表示
+    sns.heatmap(df_pivot, cmap='Blues', annot=False)
+    # sns.heatmap(df_pivot, cmap='Blues', annot=True, fmt='.2f')
+    plt.gca().xaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
     plt.title('r1={0:.2g} r2={1:.2g} w1=xx w2=xx gen={2:.2g}'.format(3, 6, 30), fontsize=9)
     plt.savefig('../data/' + para1_name + '_' + para2_name + '.png')
     plt.show()
@@ -102,21 +135,24 @@ def show_list_YP(YP, width=5, height=5):
 
 
 def change_r1_w1_YP():
-    a = np.arange(3, 4, 0.2)
-    b = np.arange(0.5, 1.0, 0.1)
-    fig, ax = plt.subplots(ncols=a.size, nrows=b.size,
+    gen = 30
+    a = np.arange(3, 6, 1)
+    b = np.arange(0.9, 1.3, 0.1)
+    fig, ax = plt.subplots(ncols=b.size, nrows=a.size,
                            sharex="col", sharey="all",
                            facecolor="lightgray")
+    fig.suptitle('r1=xx r2=r1*2 w1=xx w2={0:.2g} gen={1:.2g}'.format(-0.3, gen), fontsize=9)
     for aline, r1 in zip(ax, a):
         for elem, w1 in zip(aline, b):
             YP = Young_Pattern(r1, r1 * 2, w1, -0.25, 0.08)
             YP.init_state(50, 50)
-            elem.imshow(YP.far_generation(20), cmap='pink')
+            elem.imshow(YP.far_generation(gen), cmap='pink')
+            elem.set_title("r1={0:.2g} w1={1:.2g}".format(r1, w1), fontsize=7)
     plt.show()
 
 
 def main():
-    heat_map('w1', np.arange(0.7, 1.3, 0.1), 'w2', np.arange(-0.5, -0., 0.1))
+    heat_map('w1', np.arange(0, 30, 0.5), 'w2', np.arange(-10.0, 0., 0.5))
     # change_r1_w1_YP()
 
     # change_r1_w1_YP()
