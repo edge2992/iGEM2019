2620dcbbfb11f21ba23ee32168ec40f43e38c6a0
kaz-i-54 <torune.summer@gmail.com>
2019-08-31 16:32:14 +0900

    画像を比較する

--- Image_Comparison/compare.py
+++ Image_Comparison/compare.py
@@ -4,7 +4,42 @@ import math
 import matplotlib.pyplot as plt
 
 
-def compare_two(originalfile, comparefile):
+def compare_two_from_cv2(init, comp):
+    # 画素値の読み込み
+    pixel_value_Ori = init.flatten().astype(float)
+    pixel_value_Dis = comp.flatten().astype(float)
+
+    # 画素情報の取得
+    imageHeight, imageWidth, BPP = init.shape
+
+    # 画素数
+    N = imageHeight * imageWidth
+
+    # 1画素あたりRGB3つの情報がある.
+    addr = N * BPP
+
+    # RGB画素値の差の2乗の総和
+    sumR = 0
+    sumG = 0
+    sumB = 0
+
+    # 差の2乗の総和を計算
+    for i in range(addr):
+        if i % 3 == 0:
+            sumB += pow((pixel_value_Ori[i] - pixel_value_Dis[i]), 2)
+        elif i % 3 == 1:
+            sumG += pow((pixel_value_Ori[i] - pixel_value_Dis[i]), 2)
+        else:
+            sumR += pow((pixel_value_Ori[i] - pixel_value_Dis[i]), 2)
+
+        # PSNRを求める
+    MSE = (sumR + sumG + sumB) / (3 * N)
+    PSNR = 10 * math.log(255 * 255 / MSE, 10)
+    print('PSNR', PSNR)
+    return PSNR
+
+
+def compare_two_from_file(originalfile, comparefile):
 
     try:
         # 画像の読み込み
@@ -56,7 +91,7 @@ def main():
     for w1 in w1_list:
         print("w1={0:.2g}".format(w1))
         comparefile = 'image_data/r1=3_r2=6_w1={0:.2g}_w2=-3_gen=30.png'.format(w1)
-        result.append(compare_two(originalfile, comparefile))
+        result.append(compare_two_from_file(originalfile, comparefile))
     fig = plt.figure()
     ax = fig.add_subplot(111, xlabel='w1', ylabel='PSNR')
     ax.bar(w1_list, result)
